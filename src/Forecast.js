47a0bb5caf0712cbd265c8c88a9f50323de63453
Veronica <vveronikanika@gmail.com>
2022-08-11 22:01:20 +0300

    fix: correct day, forecast for one day

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,35 +1,28 @@
 import React, {useState} from "react";
 import "./Forecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 
 export default function Forecast(props) {
-
-	// console.log(props.coordinates);
-
 	const [loaded, setLoaded] = useState(false);
 	const [forecast, setForecast] = useState(null);
 
 	function showForecast(response){
-		// console.log(forecast);
+		console.log(response);
+		setLoaded(true);
 		setForecast(response.data.daily);
 
-		setLoaded(true);
-		// console.log(forecast);
 	}
 
 	if (loaded){
-		const icon = `icons/${forecast[0].weather[0].icon}.svg`;
+			console.log(forecast);
 		return (
-			<div className="weather_forecast Forecast" id="forecast">
-				<div className="col-2 current_info forecast_week_day pt-2 pb-2">
-					{forecast[0].dt}
-					<img src={icon} alt="cloudy" />
-					<div className="forecast_temperature row">
-						<span>&#9650; {Math.round(forecast[0].temp.max)}°</span>
-						<span>&#9660; {Math.round(forecast[0].temp.min)}°</span>
+			<div className="weather_forecast Forecast row">
+					<div className="col-2 current_info forecast_week_day pt-2 pb-2">
+						<WeatherForecastDay data={forecast[0]}/>
 					</div>
-				</div>
 			</div>
+			
 		);
 	} else {
 		const apiKey = "0f146129869d8f01315497362c4058ae";
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function WeatherForecastDay(props){
+
+	function date(){
+		const data = new Date(props.data.dt * 1000);
+		const day = data.getDay();
+		const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		return week[day];
+	}
+	const icon = `icons/${props.data.weather[0].icon}.svg`;
+	return (
+		<div>
+			<div>{date()}</div>
+			<img src={icon} alt="cloudy" />
+			<div className="forecast_temperature row">
+				<span>&#9650; {Math.round(props.data.temp.max)}°</span>
+				<span>&#9660; {Math.round(props.data.temp.min)}°</span>
+			</div>
+		</div>
+	);
+}
